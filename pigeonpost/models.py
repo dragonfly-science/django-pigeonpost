500bcca08c0e0b8ac0b891513e418f001ebd693a
Tim McNamara <code@timmcnamara.co.nz>
2012-03-28 14:26:30 +1300

    bugfixes: code now runs

--- pigeonpost/models.py
+++ pigeonpost/models.py
@@ -6,7 +6,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.db import models
 from django.dispatch import Signal, receiver
-from django.contrib.auth import User
 
 
 class ContentQueue(models.Model):
@@ -16,9 +15,9 @@ class ContentQueue(models.Model):
     successes = models.IntegerField(null=True, blank=True)
     failures = models.IntegerField(null=True, blank=True)
     send = models.BooleanField(default=True)
-    sent = models.DatetimeField(null=True, blank=True)
-    render_email = TextField()
-    email_user = TextField()
+    sent = models.DateTimeField(null=True, blank=True)
+    render_email = models.TextField()
+    email_user = models.TextField()
     schedule_time = models.DateTimeField()
 
     class Meta:
@@ -38,7 +37,7 @@ class Outbox(models.Model):
 email_signal = Signal(providing_args=['render_email', 'email_user', 'schedule_time'])
 
 @receiver(email_signal)
-def add_to_queue(sender, render_email='render_email', email_user='email_user', schedule_time=None):
+def add_to_queue(sender, render_email='render_email', email_user='email_user', schedule_time=None, **kwargs):
     if not schedule_time:
         schedule_time = datetime.datetime.now()
     try:
@@ -70,11 +69,11 @@ def send_email():
                         failures += 1
                     except:
                         pass
-         # Now make a record
-         sendable.successes = successes
-         sendable.failures = failures
-         sendable.sent=datetime.datetime.now()
-         seandable.save()
+        # Now make a record
+        sendable.successes = successes
+        sendable.failures = failures
+        sendable.sent=datetime.datetime.now()
+        seandable.save()
 
                     
                     
