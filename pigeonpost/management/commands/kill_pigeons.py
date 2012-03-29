1457577cf77d7c1866708aa0c983c18d4b57db0d
Edward Abraham <edward@dragonfly.co.nz>
2012-03-30 09:34:25 +1300

    Made a kill_pigeons panic command

---
+++ pigeonpost/management/commands/kill_pigeons.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+from pigeonpost.models import kill_pigeons
+
+class KillCommand(BaseCommand):
+    help = "The pigeonpost panicbutton. Stops any pigeons in the queue generating email messages"
+
+    def handle(self, *args, **options):
+        assert len(args) == 0, "We don't support any options."
+        assert len(options) == 0, "We don't support any options."
+        kill_pigeons()
