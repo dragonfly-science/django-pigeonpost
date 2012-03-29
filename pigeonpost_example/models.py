d0f4f81bc7283c279207570cecec8a93a993743b
Tim McNamara <code@timmcnamara.co.nz>
2012-03-29 17:21:55 +1300

    Provide an example of a model which supports pigeonpost

--- pigeonpost_example/models.py
+++ pigeonpost_example/models.py
@@ -1,4 +1,14 @@
+from django.core.mail import EmailMessage
 from django.db import models
 
-class DummyPost(models.Model):
+class Article(models.Model):
     text = models.TextField()
+    title = models.TextField()
+
+    def render_email(self, user):
+        """
+        render_email is expected to return None or a properly formed EmailMessage
+        """
+        if 'example.com' in user.email:
+            return EmailMessage('New Post: ' + self.title, self.text, from_email='anon@example.com', to=[user.email]) 
+    
