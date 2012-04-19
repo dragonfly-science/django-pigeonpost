aa07dcd1cd0bbbebfffb6351804fad7755e2cbb3
Tim McNamara <code@timmcnamara.co.nz>
2012-04-20 10:58:00 +1200

    Signal receivers now accept kwargs

--- pigeonpost/tasks.py
+++ pigeonpost/tasks.py
@@ -54,7 +54,7 @@ def process_outbox(max_retries=3, pigeon=None):
         connection.close()
 
 @receiver(pigeonpost_immediate)
-def add_immediate_message_to_outbox(sender, message, user):
+def add_immediate_message_to_outbox(sender, message, user, **kwargs):
     Outbox(message=pickle.dumps(message), user=user).save()
 
 @receiver(pigeonpost_queue)
