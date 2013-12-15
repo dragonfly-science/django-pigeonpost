89a6b2dca6038884d71c88103e6460014b41fd79
Joel Pitt <joel@joelpitt.com>
2013-12-16 11:57:14 +1300

    Make authorship belong to Dragonfly

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pigeon Post'
-copyright = u'2013, Joel Pitt, Edward Abraham, Tim McNamara'
+copyright = u'2013, Dragonfly Science'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'PigeonPost.tex', u'Pigeon Post Documentation',
-   u'Joel Pitt, Edward Abraham, Tim McNamara', 'manual'),
+   u'Dragonfly Science', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,7 +214,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'pigeonpost', u'Pigeon Post Documentation',
-     [u'Joel Pitt, Edward Abraham, Tim McNamara'], 1)
+     [u'Dragonfly Science'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,7 +228,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'PigeonPost', u'Pigeon Post Documentation',
-   u'Joel Pitt, Edward Abraham, Tim McNamara', 'PigeonPost', 'One line description of project.',
+   u'Dragonfly Science', 'PigeonPost', 'One line description of project.',
    'Miscellaneous'),
 ]
 
