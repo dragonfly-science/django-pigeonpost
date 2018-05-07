8ecd7dd9357dd4804da1f811645fc1230e57023f
Richard Mansfield <richard@dragonfly.co.nz>
2018-05-07 16:51:05 +1200

    Use new-style management command arg parsing

--- pigeonpost/management/commands/deploy_pigeons.py
+++ pigeonpost/management/commands/deploy_pigeons.py
@@ -1,18 +1,16 @@
-from optparse import make_option
-
 from django.core.management.base import BaseCommand, CommandError
 from pigeonpost.tasks import send_email
 
 class Command(BaseCommand):
     help = "Sends any pending emails in the ContentQueue"
-    option_list = BaseCommand.option_list + (
-        make_option('-d', '--dry-run',
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-d', '--dry-run',
             default=False,
             action='store_true',
             dest='dry_run',
-            help="Create, but do not send messages that have been queued. Useful to help test a model's render_email method. Logs messages at level debug with the pigeonpost.dryrun logger."
-        ),
-    )
+            help="Create, but do not send messages that have been queued. Useful to help test a model's render_email method. Logs messages at level debug with the pigeonpost.dryrun logger.")
 
     def handle(self, *args, **options):
         send_email(dry_run=options['dry_run'])
